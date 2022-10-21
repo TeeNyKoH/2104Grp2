75734ece1238a8c134eb2e37f7f5744990b37469
“TeeNyKoH” <teenykoh@gmail.com>
2022-10-22 03:49:23 +0800

    Bump detection with MPU-6050

---
+++ build/mpu6050_i2c/CMakeFiles/mpu6050_i2c.dir/Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d
@@ -0,0 +1,32 @@
+i2c/mpu6050_i2c/CMakeFiles/mpu6050_i2c.dir/Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj: \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/assert.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_ansi.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/newlib.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_newlib_version.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/config.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/features.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/stdint.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_stdint.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_float/include/pico/float.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/math.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/reent.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_ansi.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_types.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/_types.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/lock.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/cdefs.h \
+ /opt/homebrew/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/float.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /Users/Tennyson/Github_Repo/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h
