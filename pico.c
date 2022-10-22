e810b5fde6b89cbbf7c0c9845d1f8a1360ff0b32
catifier <94165084+catifier@users.noreply.github.com>
2022-10-22 10:34:27 +0800

    Allow variable length messages

--- pico.c
+++ pico.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
-#define messageLength 49
 static int addr = 0x55;
 
-void send(char* msg);
+void send(char* msg, int length);
 
 int main() {
     printf("Pico Pins: %d, %d\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
@@ -22,17 +21,17 @@ int main() {
     
     char pico_ad[50] = "Raspberry Pi Pico\0";
     while(1){
-      send(pico_ad);
+      send(pico_ad, 50);
       sleep_ms(5000);
     }
 }
 
-void send(char* msg){
-  uint8_t tmp[messageLength + 1];
-  memset(tmp, '\0', messageLength + 1);
-  for(int i = 0; i < (messageLength - 1); i++){
+void send(char* msg, int length){
+  uint8_t *tmp = (uint8_t *) malloc(length + 1);
+  memset(tmp, '\0', length + 1);
+  for(int i = 0; i < length; i++){
     tmp[i] = (uint8_t)msg[i];
   }
-  tmp[messageLength] = '\0';
-  i2c_write_blocking(i2c_default, addr, tmp, 50, false);
+  tmp[length] = '\0';
+  i2c_write_blocking(i2c_default, addr, tmp, length + 1, false);
 }
