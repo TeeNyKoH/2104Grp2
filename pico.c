439bd46ca32c291538d06ff4310f7baed196c070
catifier <94165084+catifier@users.noreply.github.com>
2022-11-10 01:43:07 +0800

    Test changes

--- pico.c
+++ pico.c
@@ -3,27 +3,35 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+#define MAX_LENGTH 50
 
 static int addr = 0x55;
 
 void send(char* msg, int length);
+void receive(uint8_t* buf);
 
 int main() {
-    printf("Pico Pins: %d, %d\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
-    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+  printf("Pico Pins: %d, %d\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
+  // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+  i2c_init(i2c_default, 100 * 1000);
+  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+  // Make the I2C pins available to picotool
+  bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
     
-    char pico_ad[50] = "Raspberry Pi Pico\0";
-    while(1){
-      send(pico_ad, 50);
-      sleep_ms(5000);
-    }
+  uint8_t *buf = (uint8_t *) malloc(MAX_LENGTH + 1);
+  memset(buf, '\0', MAX_LENGTH + 1);
+    
+  char pico_ad[50] = "Raspberry Pi Pico\0";
+  while(1){
+    send(pico_ad, 50);
+    sleep_ms(5000);
+    memset(buf, '\0', MAX_LENGTH + 1);
+    receive(buf);
+    sleep_ms(5000);
+  }
 }
 
 void send(char* msg, int length){
@@ -35,4 +43,8 @@ void send(char* msg, int length){
   tmp[length] = '\0';
   i2c_write_blocking(i2c_default, addr, tmp, length + 1, false);
   free(tmp);
+}
+
+void receive(uint8_t* buf){
+  i2c_read_blocking(i2c_default, addr, buf, MAX_LENGTH, false);
 }
