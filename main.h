b2285118eb1ef3cc0460f82220424ba8f9d5c3a6
catifier <94165084+catifier@users.noreply.github.com>
2022-11-26 06:16:08 +0800

    Add comms defines

--- main.h
+++ main.h
@@ -3,4 +3,12 @@
 
 extern int trigPin;
 extern int echoPin1, echoPin2, echoPin3;
+
+#define UART_INSTANCE (uart0)
+#define UART_TX 12
+#define UART_RX 13
+#define UART_SPEED 115200
+#define DATA_BIT 8
+#define STOP_BIT 1
+
 #endif
