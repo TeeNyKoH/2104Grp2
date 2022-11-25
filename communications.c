814a978405483653eb889c9de7242e0566c6ea2c
catifier <94165084+catifier@users.noreply.github.com>
2022-11-26 05:58:18 +0800

    Create communications.c

---
+++ communications.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "communications.h"
+#include "main.h"
+
+char buffer[BUFFER_SIZE];
+static int mqtt_printStatus = 0;
+char mqtt[MAX_LENGTH];
+static int mqtt_length = 0;
+
+// Directly print a statement to MQTT
+void uartprintf(const char* fmt, ...){
+  memset(buffer, '\0', BUFFER_SIZE);
+  va_list args;
+  va_start(args, fmt);
+  int msgLength = vsnprintf(buffer, sizeof(buffer), fmt, args);
+  va_end(args);
+  buffer[msgLength] = EOT;
+  uart_write_blocking(UART_INSTANCE, buffer, msgLength + 1);
+}
+
+// For printing long messages that require multiple "printf" 's
+void readyPrintf(){
+  //Clear previous messages
+  memset(mqtt, '\0', MAX_LENGTH);
+  mqtt_length = 0;
+  mqtt_printStatus = 1;
+}
+
+void long_printf(const char* fmt, ...){
+  if (mqtt_printStatus == 0){
+    // Did not call readyPrintf first, so return.
+    return;
+  }
+  // Keep concatenating strings
+  va_list args;
+  va_start(args, fmt);
+  memset(buffer, '\0', BUFFER_SIZE);
+  int msgLength = vsnprintf(buffer, sizeof(buffer), fmt, args);
+  va_end(args);
+  
+  if (mqtt_length == 0){
+    strncpy(mqtt, buffer, msgLength);
+    mqtt_length += msgLength;
+  } else if (mqtt_length + msgLength < MAX_LENGTH){
+    strncat(mqtt, buffer, msgLength);
+    mqtt_length += msgLength;
+  } else {
+    printf("long_printf overflow!\n");
+  }
+}
+
+void stopPrintf(){
+  if (mqtt_printStatus == 0){
+    // Did not call readyPrintf first, so return.
+    return;
+  }
+  // Attach EOT and send
+  mqtt[mqtt_length] = EOT;
+  ++mqtt_length;
+  uart_write_blocking(UART_INSTANCE, mqtt, mqtt_length);
+  mqtt_printStatus = 0;
+}
+
+void receive(){
+  
+  if (!uart_is_readable(UART_INSTANCE)){
+      return;
+  }
+  
+  static char ch;
+  static int charCount = 0;
+  
+  memset(buffer, '\0', BUFFER_SIZE);
+  
+  // Check for message header 0x01 (Start of Header)
+  ch = uart_getc(UART_INSTANCE);
+  if (ch != 0x01){
+    return;
+  }
+  
+  while(uart_is_readable(UART_INSTANCE)){
+    ch = uart_getc(UART_INSTANCE);
+    // Check and continue if not 0x04 (End of Transmission)
+    if(ch != EOT){
+      buffer[charCount] = ch;
+      charCount++;
+    } 
+  }
+  
+  if (charCount == 8){
+    if (strncmp(buffer, "GOTO", 4) == 0){
+      if(isdigit(buffer[5]) && isdigit(buffer[7])){
+        int x = buffer[5] - '0';
+        int y = buffer[7] - '0';
+        
+        //Insert mapping goto here.
+        uartprintf("COMMAND RECEIVED: GOTO %d %d\n", x, y);
+      }
+    }      
+  }
+  
+  charCount = 0;
+}
